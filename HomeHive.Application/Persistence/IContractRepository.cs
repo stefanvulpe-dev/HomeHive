d2ad03d43692bbd7532fc44777b6b32ef819bec4
Chichirau Razvan <chichiraurazvan@yahoo.com>
2023-12-15 18:10:13 +0200

    feat(API): Made API endpoint to retrieve user's contracts

--- HomeHive.Application/Persistence/IContractRepository.cs
+++ HomeHive.Application/Persistence/IContractRepository.cs
@@ -1,7 +1,9 @@
-﻿using HomeHive.Domain.Entities;
+﻿using HomeHive.Domain.Common;
+using HomeHive.Domain.Entities;
 
 namespace HomeHive.Application.Persistence;
 
 public interface IContractRepository : IAsyncRepository<Contract>
 {
+    Task<Result<IReadOnlyList<Contract>>> GetContractsByUserId(Guid userId);
 }
