6b68c9b7899a51ef43b933955fc48c974bd696ac
Octavian Gavril <100153009+octaviangavril@users.noreply.github.com>
2024-01-07 18:44:51 +0200

    feat(Tests): integration test example

--- Test/HomeHive.IntegrationTests/Base/JwtTokenProvider.cs
+++ Test/HomeHive.IntegrationTests/Base/JwtTokenProvider.cs
@@ -6,10 +6,10 @@ namespace IntegrationTests.Base;
 
 public static class JwtTokenProvider
 {
-    public static string Issuer { get; } = "Sample_Auth_Server";
+    public static string Issuer { get; } = "https://localhost:7082";
     public static SecurityKey SecurityKey { get; } =
         new SymmetricSecurityKey(
-            Encoding.ASCII.GetBytes("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IlJhcmUyMDAyIiwibmFtZWlkIjoiODBjZGIzNTAtNjNlMS00NWM0LTg4NmEtOWJmZWRlMjU0ZGJhIiwianRpIjoiMmQxMTUwMjUtNGFjZi00M2ZlLThkMjYtYTY1YTRjYWFlNDViIiwicm9sZSI6IlVzZXIiLCJuYmYiOjE3MDQ1NjY1MTMsImV4cCI6MTcwNDU2NzExMywiaWF0IjoxNzA0NTY2NTEzLCJpc3MiOiJodHRwczovL2xvY2FsaG9zdDo1MTQ0IiwiYXVkIjoiaHR0cHM6Ly9sb2NhbGhvc3Q6NTE0NCJ9.P6D-mHYwKaK9MDCkTPQl2pqibSukOaYKdJAUlYEtijQ")
+            Encoding.ASCII.GetBytes("7567693c464441e000f6f4150eb7ff2db7449baa25e8b369dead88967e2f841b")
         );
     
     public static SigningCredentials SigningCredentials { get; } = 
--- Test/HomeHive.IntegrationTests/Controllers/ContractsControllerTests.cs
+++ Test/HomeHive.IntegrationTests/Controllers/ContractsControllerTests.cs
@@ -1,6 +1,10 @@
-using HomeHive.Application.Features.Contracts.Queries;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using HomeHive.Application.Features.Contracts.Queries.GetAllContracts;
+using HomeHive.Application.Models;
 using IntegrationTests.Base;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace IntegrationTests.Controllers;
 
@@ -13,13 +17,33 @@ public class ContractsControllerTests: BaseApplicationContextTexts
     {
         // Arrange
         var getAllContracts = $"{BaseUrl}/all";
+        
+        var authenticateEndpoint = "/api/v1/Authentication/login";
+
+        var fakeUser = new LoginModel
+        {
+            UserName = "octav123",
+            Password = "!Scr00l33"
+        };
+
+        var loginResponse = await Client
+            .PostAsJsonAsync(authenticateEndpoint, fakeUser);
+
+        // Assert
+        loginResponse.EnsureSuccessStatusCode();
+        var stringResponse = await loginResponse.Content.ReadAsStringAsync();
+        var loginResult = JObject.Parse(stringResponse);
+
+        var token = loginResult["value"]!["accessToken"]?.Value<string>();
+        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
         var response = await Client.GetAsync(getAllContracts);
         // Act
         response.EnsureSuccessStatusCode();
         var responseString = await response.Content.ReadAsStringAsync();
-        var result = JsonConvert.DeserializeObject<List<ContractDto>>(responseString);
+        var result = JsonConvert.DeserializeObject<GetAllContractsQueryResponse>(responseString);
         // Assert
-        result?.Count().Should().Be(4);
+        result?.Contracts.Should().NotBeNullOrEmpty();
 
     }
     
