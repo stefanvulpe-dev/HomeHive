ceffa72773c495a9c225bfa4e19b7ae4ee10a907
Stefan Vulpe <svulpe25@gmail.com>
2024-01-09 21:34:32 +0200

    fix(UI, view estate): repair BuyEstateForm

---
+++ HomeHive.UI/ViewModels/Estates/EstateBuyModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeHive.UI.ViewModels.Estates;
+
+public class EstateBuyModel
+{
+    [Required(ErrorMessage = "Please enter something here")]
+    [MaxLength(250, ErrorMessage = "Your description is too long (250 characters max).")]
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Price is required")]
+    [Range(0, 1000000000, ErrorMessage = "Price must be between 0 and 1,000,000,000")]
+    public decimal Price { get; set; }
+    
+    [Required(ErrorMessage = "Contract type is required")]
+    public string? ContractType { get; set; }
+    
+    [Required(ErrorMessage = "Start date is required")]
+    public DateTime? StartDate { get; set; }
+    
+    public DateTime? EndDate { get; set; }
+}
