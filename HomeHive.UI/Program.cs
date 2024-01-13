be72a5caaa591ffc7ad311e06581add7a7ef98c5
Chichirau Razvan <chichiraurazvan@yahoo.com>
2024-01-13 18:52:15 +0200

    feat(UI): change home path to "/"

--- HomeHive.UI/Program.cs
+++ HomeHive.UI/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddScoped<IContractDataService, ContractDataService>();
 builder.Services.AddHttpClient("HomeHive.API",
         client =>
         {
-            client.BaseAddress = new Uri(builder.Configuration["HomeHive.API:BaseAddress"] ?? "https://localhost:5001");
+            client.BaseAddress = new Uri(builder.Configuration["HomeHive.API:BaseAddress"] ?? "https://localhost:7082");
         })
     .ConfigurePrimaryHttpMessageHandler(services =>
     {
