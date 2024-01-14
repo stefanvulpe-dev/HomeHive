ba12089d09e44413c06697f6f7a6c56200d69248
daria <dariae9@yahoo.com>
2024-01-14 14:37:48 +0200

    feat(UI): search bar is functional on every page

--- HomeHive.UI/ViewModels/Estates/SearchModel.cs
+++ HomeHive.UI/ViewModels/Estates/SearchModel.cs
@@ -3,7 +3,4 @@ namespace HomeHive.UI.ViewModels.Estates;
 public class SearchModel
 {
     public string? Name { get; set; }
-    public string? Location { get; set; }
-    public int NumberOfPeople { get; set; }
-    public string? EstateCategory { get; set; }
 }
